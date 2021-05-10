2857f6325480f1260ff28b8594601734d7577fa2
katesky <kateskytech@gmail.com>
2021-05-10 16:39:43 -0500

    1 file modified

--- src/app/active-item.directive.ts
+++ src/app/active-item.directive.ts
@@ -5,8 +5,12 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 })
 export class ActiveItemDirective {
   defaultColor: string;
-  @HostListener('click') active() {
-    this.setActiveColor();
+  @HostListener('mouseenter') active() {
+   this.el.nativeElement.style.backgroundColor = "yellow";
+  }
+   @HostListener('mouseleave') deactive() {
+       this.el.nativeElement.style.backgroundColor = this.defaultColor;
+
   }
   setActiveColor() {
     this.el.nativeElement.style.backgroundColor = this.el.nativeElement.classList.contains(
